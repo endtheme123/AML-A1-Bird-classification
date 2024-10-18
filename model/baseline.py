3617ec4c51760454a449181475d3439c1c310b42
endtheme123 <endtheme1212@gmail.com>
2024-10-18 13:00:56 +0700

    for server

--- model/baseline.py
+++ model/baseline.py
@@ -2,13 +2,25 @@ import torch.nn as nn
 import torchvision.models as models
 
 class BirdClassifier(nn.Module):
-    def __init__(self, num_classes=200):
+    def __init__(self, num_classes=200, freeze = False):
         super(BirdClassifier, self).__init__()
         # Use a pre-trained ResNet and fine-tune it
         self.model = models.resnet50(pretrained=True)
+        
         num_features = self.model.fc.in_features
         # Replace the last fully connected layer
-        self.model.fc = nn.Linear(num_features, num_classes)
+        self.model.fc = nn.Sequential(
+            nn.Linear(in_features=num_features, out_features=2048,bias=True),
+            nn.SiLU(),
+            nn.Dropout(),
+            nn.Linear(in_features=2048, out_features=num_classes,bias=True)
+        )
+
+        if freeze:
+            for param in self.model.parameters():
+                param.requires_grad = False
+            for param in self.model.fc.parameters():
+                param.requires_grad = True
 
     def forward(self, x):
         return nn.Softmax(dim = 1)(self.model(x))
--- trainer.py
+++ trainer.py
@@ -11,7 +11,7 @@ class Trainer:
         self.test_loader = test_loader
         # self.val_loader = val_loader
         self.criterion = nn.CrossEntropyLoss()
-        self.optimizer = optim.Adam(self.model.parameters(), lr=0.001)
+        self.optimizer = optim.Adam(self.model.model.fc.parameters(), lr=0.001)
         self.device = device
         self.output_dir="./checkpoints"
         self.result_dir="./results"
@@ -31,11 +31,8 @@ class Trainer:
         for epoch in tqdm(range(epochs)):
             self.model.train()
             running_loss = 0.0
-            gt = []
-            pred = []
+            running_corrects = 0
 
-            
-            count = 0
             print()
             for (inputs, labels) in self.train_loader:
                 
@@ -53,16 +50,17 @@ class Trainer:
 
                 running_loss += loss.item()
                 _, predicted = torch.max(outputs, 1)
-                # print("predicted: ",predicted)
-                # print("labels: ",labels)
-                gt.extend(labels)
-                pred.extend(predicted)
-                count = count+1
+
+                running_corrects += sum(1 for a, b in zip(predicted, labels) if a == b)
+                print("output: ",outputs)
+                print("predicted: ",predicted)
+                print("labels: ",labels)
                 # total += labels.size(0)
                 # correct += (predicted == labels).sum().item()
-            training_loss = running_loss
+            training_loss = running_loss/len(self.train_loader.dataset)
             training_loss_list.append(training_loss)
-            train_accuracy = 100*sum([1 for g, p in zip(gt, pred) if g == p]) / len(gt)
+            
+            train_accuracy = running_corrects/len(self.train_loader.dataset)
             training_acc_list.append(train_accuracy)
 
             val_loss, val_acc = self.validate()
@@ -105,8 +103,7 @@ class Trainer:
     def validate(self):
         self.model.eval()
         valid_loss = 0.0
-        count = 0
-        gt = []
+        valid_corrects = 0
         pred = []
         with torch.no_grad():
             for (data, target) in self.test_loader:
@@ -115,16 +112,13 @@ class Trainer:
                 output = self.model(data)
                 preds = torch.argmax(output, axis=1).tolist()
                 labels = target.tolist()
-                gt.extend(labels)
-                pred.extend(preds)
                 loss = self.criterion(output, target)
-                count = count+1
+                # count = count+1
                 # update-average-validation-loss
                 valid_loss += loss.item()
-            val_loss = valid_loss/count
-            print(len(gt))
-            print(sum([1 for g, p in zip(gt, pred) if g == p]))
-            val_accuracy = 100*sum([1 for g, p in zip(gt, pred) if g == p]) / len(gt)
+                valid_corrects += sum(1 for a, b in zip(preds, labels) if a == b)
+            val_loss = valid_loss/len(self.test_loader.dataset)
+            val_accuracy = valid_corrects/len(self.test_loader.dataset)
         #     correct = 0
         #     total = 0
         # with torch.no_grad():
